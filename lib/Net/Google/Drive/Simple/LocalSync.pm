87323b938293759db040033f0940126755f406c9
Stein Hammer <steihamm@gmail.com>
2020-07-05 11:04:56 +0200

    Skriver ut todo

--- lib/Net/Google/Drive/Simple/LocalSync.pm
+++ lib/Net/Google/Drive/Simple/LocalSync.pm
@@ -301,6 +301,7 @@ sub mirror {
 			        $conflict_bck->dirname->make_path;
 			       	move($value->{loc_pathfile}, _string2perlenc($conflict_bck->to_string));
 			       	delete $lc{$value->{loc_pathfile}};
+			       	say "TODO: LEGG INN I EN LOKAL SLETTE LISTE SÅ DET IKKE BLIR UPLOADET I EN KARANTENE PERIODE";
 					$self->db->query( "delete from files_state where loc_pathfile = ?", $value->{loc_pathfile});
 				}
 			}
